d8de32024f6a4660b52cb4a98b93bb1a82fde1de
namanshr1403 <namanshrivastava94253@gmail.com>
2023-07-21 19:49:39 +0530

    NewsApp_First_Commit

---
+++ app/src/main/java/com/example/newapi/ApiInterface.java
@@ -0,0 +1,27 @@
+package com.example.newapi;
+
+import com.example.newapi.Model.Headlines;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+
+    @GET("top-headlines")
+    Call<Headlines> getHeadlines(
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+    @GET("everything")
+    Call<Headlines> getSpecificData(
+            @Query("q") String query,
+            @Query("apiKey") String apiKey
+    );
+
+
+
+}
